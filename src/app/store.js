6ff9433ab71b04b494cf3b78b3f5a8ac59f2a58a
Ghribi01 <ghribim07@gmail.com>
2024-04-28 14:45:28 +0100

    Latest Version

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import loginSliceReducer from './features/login/loginSlice'
 import registerSliceReducer from './features/register/registerSlice'
 import appReducer from './features/appSlice/appSlice'
+import userDataReducer from './features/userData/userData'
+import eventsReducer from './features/events/events'
 
 // Configure the Redux store
 const store = configureStore({
@@ -9,6 +11,8 @@ const store = configureStore({
     app: appReducer,
     loginSlice: loginSliceReducer,
     registerSlice: registerSliceReducer,
+    userData: userDataReducer,
+    events:eventsReducer,
   },
 })
 
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Events = React.lazy(() => import('./components/Events/Events'))
 const CreateEvents = React.lazy(() => import('./components/Events/CreateEvent'))
 const User = React.lazy(() => import('./components/profile/UserProfile'))
+const Activities = React.lazy(() => import('./components/ProgramMonitoring/Activities'))
+const TaskManagment = React.lazy(() => import('./components/ProgramMonitoring/TaskManagment'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/user', name: 'User', element: User },
+  { path: '/Activities', name: 'Activities', element: Activities },
+  { path: '/TaskManagment', name: 'Task Managment', element: TaskManagment },
   { path: '/events', name: 'All events', element: Events },
   { path: '/CreateEvent', name: 'Create Events', element: CreateEvents },
 ]
